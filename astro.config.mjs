c3aa9f4ce6a2e97661d3e6ec5a6c457cfd09ca94
mfaiz6 <mfaizgr@gmail.com>
2024-09-21 17:52:56 +0530

    learn islands and complete it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://astro-learnin.netlify.app"
+  site: "https://astro-learnin.netlify.app",
+  integrations: [preact()]
 });
